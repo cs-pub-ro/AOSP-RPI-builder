c043b53f8ec99e48d9fb6e63957e005773cf1fb7
Stancu Florin <niflostancu@gmail.com>
2025-07-07 22:09:39 +0300

    user: cleanup nvim archive

--- vm/scripts/install-stage2.d/35-neovim.sh
+++ vm/scripts/install-stage2.d/35-neovim.sh
@@ -5,6 +5,7 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 rm -rf /opt/nvim*
 tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm -f nvim-linux-x86_64.tar.gz
 
 echo 'export PATH=/opt/nvim-linux-x86_64/bin:$PATH' > /etc/profile.d/30-nvim.sh
 
