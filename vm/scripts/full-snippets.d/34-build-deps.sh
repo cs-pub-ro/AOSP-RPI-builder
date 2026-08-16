c435248d3c1c9c3161046b083aefd40699974902
Stancu Florin <niflostancu@gmail.com>
2026-08-16 20:53:10 +0300

    vm: build-deps: use pkg_install, reorder some packages
    + add some missing terminal utilities

--- vm/scripts/full-snippets.d/34-build-deps.sh
+++ vm/scripts/full-snippets.d/34-build-deps.sh
@@ -3,11 +3,11 @@
 # VM install initialization
 
 # terminal tools
-apt-get install -y tmux lzma xz-utils moreutils expect lsof jq vim less \
-	rsync zsh parallel tree python3 python3-venv
+pkg_install zip unzip lzma xz-utils moreutils expect lsof jq vim less \
+	tmux rsync zsh parallel file htop tree python3 python3-pip python3-venv
 
 # build dependencies
-apt-get install -y \
+pkg_install \
     sudo nano vim wget curl ca-certificates repo bash bash-completion \
 	git gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 \
 	x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev \
@@ -17,15 +17,15 @@ apt-get install -y \
 	pkg-config python3-pip python3-venv rsync fakeroot libgmp-dev \
     cmake device-tree-compiler \
     ncurses-dev libgucharmap-2-90-dev bzip2 expat gpg \
-    bc time file bsdmainutils
+    bc time bsdmainutils
 
-apt-get install -y libusb-1.0-0-dev libbz2-dev libzstd-dev pkg-config cmake \
+pkg_install libusb-1.0-0-dev libbz2-dev libzstd-dev pkg-config cmake \
 	libssl-dev g++ zlib1g-dev libtinyxml2-dev libgnutls28-dev kmod
 
 # wireguard
-apt-get install -y wireguard-tools
+pkg_install wireguard-tools
 
-apt-get clean
+pkg_cleanup
 
 if [[ -n "$VM_FULL_UPGRADE" ]]; then
 	pkg_upgrade_all
