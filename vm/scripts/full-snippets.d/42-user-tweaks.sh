610f247936ccd355b3d33e613525a1d9f745a59b
Stancu Florin <niflostancu@gmail.com>
2026-08-16 20:18:20 +0300

    vm: migrate user config to labvm-dotfiles

--- vm/scripts/full-snippets.d/42-user-tweaks.sh
+++ vm/scripts/full-snippets.d/42-user-tweaks.sh
@@ -2,10 +2,18 @@
 [[ -n "$__INSIDE_VM_RUNNER" ]] || { echo "Only call within VM runner!" >&2; return 1; }
 # User account (student) tweaks
 
-SRC="$(realpath "$(sh_get_script_path)/..")"
+VM_SRC="$(realpath "$(sh_get_script_path)/..")"
 
-# copy default files to /etc/skel
-rsync -a --chown=root:root --chmod=755 "$SRC/etc/skel/" "/etc/skel/"
+# install dotfiles to /etc/skel/
+rsync -a --chown=root:root --chmod=755 "$VM_SRC/labvm-dotfiles/" "/etc/skel/"
+# emulate installation using XDG paths
+env HOME=/etc/skel/ XDG_CONFIG_HOME="/etc/skel/.config" \
+	XDG_DATA_HOME="/etc/skel/.local/share" \
+	XDG_CACHE_HOME="/tmp/dotfiles-cache" \
+	XDG_STATE_HOME="/tmp/dotfiles-state" \
+	"$VM_SRC/labvm-dotfiles/install.sh"
+chmod 755 "/etc/skel" -R
+
 # clone fzf
 if [[ ! -d /etc/skel/.fzf/.git ]]; then
 	git clone --depth 1 https://github.com/junegunn/fzf.git /etc/skel/.fzf/
@@ -14,21 +22,15 @@ fi
 # this will be ran with individual user privileges
 function _install_home_config() {
 	set -e
-
 	# copy skel over (since user was created previous to installing files)
 	rsync -a --chmod=750 "/etc/skel/" "$HOME/"
-	# install fzf
-	yes | "$HOME/.fzf/install"
-
-	# run zsh to install plugins
-	zsh -i -c 'source ~/.zshrc; exit 0'
 
 	# set git identities
 	git config --global user.email "$USER@armbuilder.local"
 	git config --global user.name "${USER}_builder"
 }
 
-_exported_script="$(declare -p SRC); $(declare -f _install_home_config)"
+_exported_script="$(declare -p VM_SRC); $(declare -f _install_home_config)"
 
 for u in student admin root; do
 	echo "$_exported_script; _install_home_config" | su -c bash "$u"
